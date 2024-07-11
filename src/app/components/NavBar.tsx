626efae71c5d9f9656dd02d750a15af1d01c1e76
david little <david.little@gmail.com>
2024-07-10 21:18:55 -0500

    click logo returns to '/' page

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, HStack, Text, useColorMode } from "@chakra-ui/react";
+import { Box, HStack, Link, Text, useColorMode } from "@chakra-ui/react";
 import { Logo } from "./Logo";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { Logo2 } from "./logo2";
@@ -10,14 +10,16 @@ interface Props {
 }
 
 const NavBar = ({ onSearch }: Props) => {
-  console.log("rudolph");
   const { colorMode } = useColorMode();
-
-  console.log("santa", colorMode);
   return (
     <HStack padding="10px">
       <Box color="blue" boxSize="60px">
-        <Logo2 color={colorMode === "dark" ? "white" : "red"} boxSize="65px" />
+        <Link href="/">
+          <Logo2
+            color={colorMode === "dark" ? "white" : "red"}
+            boxSize="65px"
+          />
+        </Link>
       </Box>
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
