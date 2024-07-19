505e3daba8a1ca4284a9ead6cae68110eed98fe0
david little <david.little@gmail.com>
2024-07-19 14:08:06 -0500

    update grit logo to match colors

--- src/app/components/logo2.tsx
+++ src/app/components/logo2.tsx
@@ -17,29 +17,50 @@ export const Logo2 = (props: HTMLChakraProps<"svg">) => (
         <g clipPath="url(#clipPath18)" id="g14">
           <g transform="translate(9.6582,149.5425)" id="g20">
             <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              fill="#9fa0a3"
+              fillOpacity="1"
+              stroke="none"
               id="path22"
               d="m 0,0 c 0,57.331 46.473,103.804 103.804,103.804 46.667,0 86.14,-30.793 99.211,-73.171 H 172.742 C 160.521,58.094 132.376,76.902 100.033,75.327 61.48,73.45 30.242,42.121 28.467,3.563 c -1.991,-43.273 32.502,-78.981 75.337,-78.981 30.747,0 57.194,18.397 68.938,44.785 h 30.273 C 189.944,-73.009 150.471,-103.804 103.804,-103.804 46.473,-103.804 0,-57.331 0,0"
             />
           </g>
           <path
             id="path24"
+            fillRule="evenodd"
+            fill="#9fa0a3"
+            fillOpacity="1"
+            stroke="none"
             d="m 551.253,49.651 h -29.301 v 199.781 h 29.301 z"
           />
           <g transform="translate(635.1611,224.127)" id="g26">
             <path
               id="path28"
+              fillRule="evenodd"
+              fill="#9fa0a3"
+              fillOpacity="1"
+              stroke="none"
               d="M 0,0 V 25.306 H 154.497 V 0 H 91.899 V -174.476 H 62.598 L 62.598,0 Z"
             />
           </g>
           <g transform="translate(377.5205,249.4326)" id="g30">
             <path
               id="path32"
+              fillRule="evenodd"
+              fill="#9fa0a3"
+              fillOpacity="1"
+              stroke="none"
               d="m 0,0 c 39.115,0 64.382,-24.919 64.382,-61.305 0,-34.678 -22.734,-53.526 -46.342,-57.315 l -4.57,-0.734 49.698,-80.427 H 29.126 l -60.541,102.554 26.236,0.001 c 21.866,0 38.414,14.156 38.414,36.267 0,21.865 -16.617,35.653 -38.414,35.653 H -58.013 V -199.781 H -87.314 V 0 Z"
             />
           </g>
           <g transform="translate(103.7437,162.8608)" id="g34">
             <path
               id="path36"
+              fillRule="evenodd"
+              fill="#e12727"
+              fillOpacity="1"
+              stroke="none"
               d="M 0,0 H 113.209 L 113.33,-26.637 0,-26.634 Z"
             />
           </g>
